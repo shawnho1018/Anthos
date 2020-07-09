427bf50e2d760dd30110f97902a81a74a7b0dddb
shawnho1018 <shawnho@google.com>
2020-07-09 17:32:29 +0800

    asm 1.6 on gke

--- asm-lab/1-enable-wid.sh
+++ asm-lab/1-enable-wid.sh
@@ -6,23 +6,36 @@
 # 4. Show a pod started with K8S SA could access gcloud commands
 
 source utility.sh
-gcloud container clusters update c1 --workload-pool="${PROJECT_ID}.svc.id.goog" 
-gcloud container node-pools update default-pool --workload-metadata=GKE_METADATA --cluster c1
-# A terraform sa has been created with project owner authority
+for c in $CONTEXT_C1 $CONTEXT_C2
+  do
+    # A terraform sa has been created with project owner authority
+    kubectl create serviceaccount wid-tester --namespace default --context $c
+  done
 
-kubectl create serviceaccount wid-tester --namespace default
+for c in c1 c2 
+  do 
+    echo "update workload-pool"
+    gcloud container clusters update $c --workload-pool="${PROJECT_ID}.svc.id.goog" 
+    gcloud container node-pools update default-pool --workload-metadata=GKE_METADATA --cluster $c
+  done
 
+# link GSID to KSID
+echo "Link GSID to KSID..."
 gcloud iam service-accounts add-iam-policy-binding \
-	--role roles/iam.workloadIdentityUser \
-	--member "serviceAccount:${PROJECT_ID}.svc.id.goog[default/wid-tester]" "terraform@${PROJECT_ID}.iam.gserviceaccount.com"
+       --role roles/iam.workloadIdentityUser \
+       --member "serviceAccount:${PROJECT_ID}.svc.id.goog[default/wid-tester]" "terraform@${PROJECT_ID}.iam.gserviceaccount.com" \
 
-# add annotation
-kubectl annotate serviceaccount -n default wid-tester "iam.gke.io/gcp-service-account=terraform@${PROJECT_ID}.iam.gserviceaccount.com"
+for c in $CONTEXT_C1 $CONTEXT_C2
+  do
+    # add annotation
+    kubectl annotate serviceaccount -n default wid-tester "iam.gke.io/gcp-service-account=terraform@${PROJECT_ID}.iam.gserviceaccount.com"
+    # deploy a pod to verify
+    kubectl run --rm -it \
+    --generator=run-pod/v1 \
+    --image google/cloud-sdk:slim \
+    --serviceaccount wid-tester \
+    --namespace default \
+    --context $c \
+    wid-test
+  done
 
-# deploy a pod to verify
-kubectl run --rm -it \
---generator=run-pod/v1 \
---image google/cloud-sdk:slim \
---serviceaccount wid-tester \
---namespace default \
-wid-test
