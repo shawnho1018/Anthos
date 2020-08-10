78fa137ebf490d76cf139fd415a2d2c66db02553
shawnho1018 <shawnho@google.com>
2020-08-10 16:24:20 +0800

    add network endpoint group cleanup

--- cloudrunAnthos-pubsub/97-clean-gclb.sh
+++ cloudrunAnthos-pubsub/97-clean-gclb.sh
@@ -5,5 +5,8 @@ gcloud compute target-https-proxies delete --quiet https-lb-proxy
 gcloud compute url-maps delete --quiet web-map
 gcloud compute backend-services delete --quiet my-bes --global
 gcloud compute health-checks delete --quiet http-basic-check
+NEG_NAME=$(gcloud beta compute network-endpoint-groups list \
+    | grep istio | awk '{print $1}')
+gcloud compute network-endpoint-groups delete --quiet $NEG_NAME
 echo "Static IP will not be auto-deleted. Please remove it by yourself by calling gcloud compute addresses delete --quiet hostname-server-vip"
 
